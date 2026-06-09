4f76168a4ab3fe2cad382f8f51ccde4010f98754
Luis Fernando <luis_jna@outlook.com>
2026-06-09 11:42:44 -0300

    02 - Primeiras entidades

---
+++ src/main/java/com/example/demo/entities/Course.java
@@ -0,0 +1,27 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "course")
+public class Course {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy =
+            GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String imgUri;
+    private String imgGrayUri;
+    @OneToMany(mappedBy = "course")
+    private List<Offer> offers = new ArrayList<>();
+
+}
---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.java.Log;
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Log
+@Entity
+@Table(name = "user")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+    private String password;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant created_at;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updated_at;
+
+
+}
